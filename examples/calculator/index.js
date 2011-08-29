76fd3a95c16c53cff1b5d1db1be48ad8a8a7feb6
Steven Adams <steven@consultstevo.com>
2011-08-29 14:22:29 -0700

    First commit!

---
+++ examples/calculator/index.js
@@ -0,0 +1,18 @@
+var i = require( "./calculator.bnf.js" ).interpreter;
+i.AddTokenEventByName( "expression", function( token ){
+	var left = i.SeekTokenByName( token, "number" ).text;
+	var type = i.SeekTokenByName( token, "type" ).text;
+	var right = i.SeekTokenByName( token, "number" ).text;
+	
+	//Equate
+	var equate = -1;
+	eval( "equate = " + left + type + right + ";" );
+	console.log( "File found syntax " + equate );
+} );
+
+var parserObject = require( "../../lib/parser.js" ).parser;
+var parser = new parserObject( i );
+
+parser.ParseScript( "calc.calc" );
+
+//var interpreter = new require( "./parser.js" ).languageScript( "calc.bnf" );
